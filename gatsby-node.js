662751ee1289028b136229cabdc5998827f3fa73
root <root@kali>
2020-10-31 20:09:01 +0600

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,33 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
+  });
+};
