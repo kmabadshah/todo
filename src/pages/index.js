aaa9331d2f4f925ad4e810242b36c427f108d26c
root <root@kali>
2020-11-01 08:45:01 +0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,29 +3,10 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../styles/css/index.css";
 import { Link, navigate } from "gatsby";
 import { Context } from "../components/wrapper.js";
-import postcss from "postcss";
-import autoprefixer from "autoprefixer";
-import fs from "fs";
 
 export default function Main() {
-  fs.readFile("src/app.css", (err, css) => {
-    postcss([autoprefixer])
-      .process(css, {
-        from: "../styles/css/index.css",
-        to: "../styles/css/index.css",
-      })
-      .then(result => {
-        console.log("hello");
-        fs.writeFile("../styles/css/index.css", result.css, () => true);
-        if (result.map) {
-          fs.writeFile("../styles/css/index.css.map", result.map, () => true);
-        }
-      });
-  });
-
   const oldUser = false;
   React.useEffect(() => {
-    console.log(postcss);
     if (oldUser) navigate("/login");
     else navigate("/signup");
   }, []);
