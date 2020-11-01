62f1c199d88cb3d3554f4c74cf790aa5182a467d
root <root@kali>
2020-11-01 11:18:39 +0600

    update

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -48,11 +48,11 @@ export default function Signup() {
       `;
 
       // encryption
-      const bcrypt = await import("bcrypt");
-      bcrypt.hash(data.pass, 10, async function (err, hash) {
-        data.pass = hash;
-        console.log("hello1");
-      });
+      /* const bcrypt = await import("bcrypt");
+       * bcrypt.hash(data.pass, 10, async function (err, hash) {
+       *   data.pass = hash;
+       *   console.log("hello1");
+       * }); */
 
       console.log("hello2");
       /* const res = await client.request(query, data); */
