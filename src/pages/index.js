ad3f67ff0e4f9e79651cc1a0ea9f285cae4ca4bd
root <root@kali>
2020-11-01 08:16:01 +0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../styles/css/index.css";
 import { Link, navigate } from "gatsby";
 import { Context } from "../components/wrapper.js";
+import postcss from "postcss";
 
+console.log(postcss);
 export default function Main() {
   const oldUser = false;
   React.useEffect(() => {
