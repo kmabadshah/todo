19227042ede49cd0599272cdd76ae3f49e6a5979
root <root@kali>
2020-11-01 08:40:01 +0600

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
+  const config = getConfig();
+  config.node = {
+    fs: "empty",
+  };
+};
