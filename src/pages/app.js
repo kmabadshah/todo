6871edd863ec36fd99307892f0bb599c4e5e03fa
root <root@kali>
2020-11-02 17:56:01 +0600

    update

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Router } from "@reach/router";
 import App from "../components/app";
+import Layout from "../components/layout";
 
 export default function Users() {
   return (
-    <Router basepath="/users">
-      <App path="/" />
-    </Router>
+    <Layout>
+      <Router basepath="/users">
+        <App path="/" />
+      </Router>
+    </Layout>
   );
 }
