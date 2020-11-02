b0e1fd1d4b786fcff73e69219d41d0cbea32af8d
root <root@kali>
2020-11-02 19:51:01 +0600

    update

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Context } from "../components/wrapper";
 
 export default function User() {
+  const { currentUser } = React.useContext(Context);
+
   return (
     <div id="user">
       <h1>Hello World</h1>
