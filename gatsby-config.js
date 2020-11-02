cb7c6fe255d830f51ec1a25a9f9d7d8936653bcf
root <root@kali>
2020-11-02 18:03:01 +0600

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
 
     {
       resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/app/*`] },
+      options: { prefixes: [`/users/*`] },
     },
   ],
 };
