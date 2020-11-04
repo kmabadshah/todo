f183b394a5d3982a756f5927df1ae9d74abf1b8d
root <root@kali>
2020-11-04 19:24:01 +0600

    update

--- src/components/todocard.js
+++ src/components/todocard.js
@@ -4,7 +4,7 @@ import { BsCheck } from "react-icons/bs";
 export default function TodoCard() {
   return (
     <div id="todocard">
-      <div className="card d-flex">
+      <div id="card">
         <input type="text" id="init_input" />
         <button id="btn-submit">
           <BsCheck />
