024652454e26076add5d9c8d00e3e83d5184f077
root <root@kali>
2020-11-04 08:53:01 +0600

    update

--- src/pages/user.js
+++ src/pages/user.js
@@ -7,7 +7,7 @@ export default function User() {
   const { currentUser } = React.useContext(Context);
 
   return (
-    <div id="user" className="d-flex flex-column border border-danger ">
+    <div id="user" className="d-lex flex-column" style={{ height: "100vh" }}>
       <Navbar />
       <TodoCard />
     </div>
