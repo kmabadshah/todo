8348d20e870c7fd08a7c4641633632e546487a64
root <root@kali>
2020-11-04 19:11:01 +0600

    update

--- src/components/todocard.js
+++ src/components/todocard.js
@@ -4,8 +4,9 @@ export default function TodoCard() {
   return (
     <div id="todocard">
       <div className="card">
-        <div className="card-body">
+        <div className="card-body d-flex">
           <input type="text" />
+          <button>Click me</button>
         </div>
       </div>
     </div>
