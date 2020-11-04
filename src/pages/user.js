a51b1e427b0b20f3d717658297c48724424cd86d
root <root@kali>
2020-11-04 08:15:01 +0600

    update

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Context } from "../components/wrapper";
+import Navbar from "../components/navbar";
+import TodoCard from "../components/todocard";
 
 export default function User() {
   const { currentUser } = React.useContext(Context);
