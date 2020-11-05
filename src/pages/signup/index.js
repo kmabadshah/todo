bdaf6f3d021afef54aacf73a3c82ac234dfbb088
root <root@kali>
2020-11-05 18:11:01 +0600

    update

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -112,6 +112,7 @@ export default function Signup() {
         errors,
         register,
         err_msgs,
+        Layout,
       }}
     />
   );
--- src/pages/signup/view.js
+++ src/pages/signup/view.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Layout from "../../components/layout.js";
 
 export default function View({
   data: {
