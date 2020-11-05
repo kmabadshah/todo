b00e4e345a67421b16e8e0a8d35eda8fcdf134a7
root <root@kali>
2020-11-05 12:41:01 +0600

    update

--- src/components/todocard.js
+++ src/components/todocard.js
@@ -5,16 +5,14 @@ export default function TodoCard() {
   const [submitted, setSubmitted] = React.useState(false);
   const [todoText, setTodoText] = React.useState("");
 
-  // React.useEffect(() => {
-  //   console.log(todoText);
-  // }, [todoText]);
+  function handleSubmit(e) {
+    const data = new FormData(e.traget);
+    console.log(data);
+  }
 
   return (
     <div id="todocard">
-      <form
-        id="card"
-        onSubmit={e => e.preventDefault() & console.log(e.target.value)}
-      >
+      <form id="card" onSubmit={e => handleSubmit(e)}>
         <input type="text" id="init-input" placeholder="I will meditate..." />
         <button id="btn-submit">
           <BsCheck />
