88eedbca2fe7d004fe836a70f736d6a8216118ee
root <root@kali>
2020-11-05 12:32:01 +0600

    update

--- src/components/todocard.js
+++ src/components/todocard.js
@@ -5,16 +5,18 @@ export default function TodoCard() {
   const [submitted, setSubmitted] = React.useState(false);
   const [todoText, setTodoText] = React.useState("");
 
-  React.useEffect(() => {
-    console.log(todoText);
-  }, [todoText]);
+  // React.useEffect(() => {
+  //   console.log(todoText);
+  // }, [todoText]);
 
   return (
     <div id="todocard">
       <div id="card">
         <input
           type="text"
-          onChange={e => setTodoText(e.target.value)}
+          onChange={e =>
+            setTodoText(e.target.value) && console.log(e.target.id)
+          }
           onKeyDown={e => e.key === "Enter" && setSubmitted(true)}
           id="init-input"
           placeholder="I will meditate..."
