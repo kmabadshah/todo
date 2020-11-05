b6651923f7dd4b29c4710d9b087980221e8d2b0f
root <root@kali>
2020-11-05 12:15:01 +0600

    update

--- src/components/todocard.js
+++ src/components/todocard.js
@@ -4,12 +4,16 @@ import { BsCheck } from "react-icons/bs";
 export default function TodoCard() {
   const [submitted, setSubmitted] = React.useState(false);
 
+  const handleChange = e => {
+    console.log(e.key);
+  };
+
   return (
     <div id="todocard">
       <div id="card">
         <input
           type="text"
-          onChange={e => console.log(e.target.value)}
+          onKeydown={e => handleChange(e)}
           id="init-input"
           placeholder="I will meditate..."
         />
