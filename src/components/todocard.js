db32886d00c4c7044c5182b7048e008c147de2b8
root <root@kali>
2020-11-05 13:55:01 +0600

    update

--- src/components/todocard.js
+++ src/components/todocard.js
@@ -1,42 +1,50 @@
 import React from "react";
 import { BsCheck } from "react-icons/bs";
+import { Context } from "./wrapper";
 
 export default async function TodoCard() {
   const [submitted, setSubmitted] = React.useState(false);
   const [todoText, setTodoText] = React.useState("");
+  const { token, currentUser } = React.useContext(Context);
 
-  // async function getData() {
-  //   const imp = await import("wrapper");
-  //   console.log(React.useContext(imp).token);
-  // }
+  React.useEffect(
+    () =>
+      (async () => {
+        if (submitted) {
+          const { GraphQLClient: glClient, gql, request } = await import(
+            "graphql-request"
+          );
 
-  useSomething();
-  // React.useEffect(() => (async () => {
-  //   if (submitted) {
-  //     const { GraphQLClient: glClient, gql, request } = await import(
-  //       "graphql-request"
-  //     );
+          const client = new glClient(`${api}/graphql`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
 
-  //     const client = new glClient(`${api}/graphql`, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
+          const query = gql`
+            mutation($id: ID!) {
+              updateTodoer(
+                input: { where: { id: $id }, data: { todos: $todos } }
+              ) {
+                todoer {
+                  uname
+                  id
+                }
+              }
+            }
+          `;
 
-  //     const query = gql`
-  //       mutation($uname: String!, $pass: String!) {
-  //         createTodoer(input: { data: { uname: $uname, pass: $pass } }) {
-  //           todoer {
-  //             uname
-  //             id
-  //           }
-  //         }
-  //       }
-  //     `;
+          console.log(currentUser);
 
-  //     setTodoText("");
-  //   }
-  // })(), [submitted]);
+          // const data = {
+          //   id: currentUser.createTodoer.id
+          // };
+
+          setTodoText("");
+        }
+      })(),
+    [submitted]
+  );
 
   return (
     <div id="todocard">
@@ -57,10 +65,4 @@ export default async function TodoCard() {
       </div>
     </div>
   );
-  async function useSomething(func) {
-    const { Context } = await import("./wrapper");
-    const { token } = React.useContext(Context);
-    console.log(token);
-    // React.useEffect(func);
-  }
 }
