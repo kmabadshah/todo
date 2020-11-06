3ccf82d5cab7880d2d930f7eb2a4b5ea59e2253f
root <root@kali>
2020-11-06 08:35:01 +0600

    update

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -42,7 +42,6 @@ export default function Signup() {
         mutation($uname: String!, $pass: String!) {
           createTodoer(input: { data: { uname: $uname, pass: $pass } }) {
             todoer {
-              uname
               id
               todos {
                 text
