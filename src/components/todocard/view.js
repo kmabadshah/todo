e89cc8076b41d749b0c53768495d44fbd4416146
root <root@kali>
2020-11-06 08:43:01 +0600

    update

--- src/components/todocard/view.js
+++ src/components/todocard/view.js
@@ -7,21 +7,26 @@ export default function View({
   return (
     <div id="todocard">
       <div id="card">
-        <input
-          type="text"
-          onChange={e => setTodoText(e.target.value)}
-          onKeyDown={e =>
-            e.key === "Enter" && todoText
-              ? setSubmitted(true)
-              : setSubmitted(false)
-          }
-          id="todo-text"
-          value={todoText}
-          placeholder="I will meditate..."
-        />
-        <button id="btn-submit" onClick={() => todoText && setSubmitted(true)}>
-          <BsCheck />
-        </button>
+        <div id="todo-input">
+          <input
+            type="text"
+            onChange={e => setTodoText(e.target.value)}
+            onKeyDown={e =>
+              e.key === "Enter" && todoText
+                ? setSubmitted(true)
+                : setSubmitted(false)
+            }
+            id="todo-text"
+            value={todoText}
+            placeholder="I will meditate..."
+          />
+          <button
+            id="btn-submit"
+            onClick={() => todoText && setSubmitted(true)}
+          >
+            <BsCheck />
+          </button>
+        </div>
 
         {currentUser.todos.map(todo => {
           return (
