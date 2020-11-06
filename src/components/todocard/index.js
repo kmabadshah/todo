0b8cf2f57c297d9cf5344383c79adfb56b61cec1
root <root@kali>
2020-11-06 10:10:01 +0600

    update

--- src/components/todocard/index.js
+++ src/components/todocard/index.js
@@ -35,6 +35,25 @@ export default function TodoCard() {
   );
 }
 
+/* function cmpArr(arr1, arr2) {
+ *   arr1.map(item1 => arr2.map(item2 => {for (key, value) of item1 {
+ *     for (key, value) of item2 {
+ *
+ *     }
+ *   }}))
+ * } */
+
+function cmpObj(obj1, obj2) {
+  let eq = false;
+  for (let [k1, v1] of Object.entries(obj1)) {
+    for (let [k2, v2] of Object.entries(obj2)) {
+      eq = v1 === v2 && k1 === k2;
+    }
+  }
+
+  return eq;
+}
+
 /*
  *   React.useEffect(() => {
  *     (async () => {
